7b02012f4a6d42b6525fc99f8509c0e3ad6884f1
mwturner611 <mwturner611@gmail.com>
2021-10-17 17:17:36 -0500

    Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
+import Button from './Button';
+
+
 const Header = () => {
+    const onClick = () => {
+        console.log('Click')
+        
+    }
     return (
-        <header>
+        <header className='header'>
             <h1>Task Tracker</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
+
+
 
 export default Header
