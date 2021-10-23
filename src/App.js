e96742bd53f4b67a91791f1ad7ff28f80c573ccf
mwturner611 <mwturner611@gmail.com>
2021-10-23 10:14:26 -0500

    Delete task function

--- src/App.js
+++ src/App.js
@@ -23,11 +23,17 @@ function App() {
         day: 'Feb 5th at 2:30pm',
         reminder: false,
     }
-])
+]);
+
+// Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : "No Tasks to Show"}
     </div>
   );
 }
